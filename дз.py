1c1432e15a4f062de27675202d88a3833f786f93
Kommandor <a.tsymbaliuk2007@gmail.com>
2023-05-12 00:09:36 +0300

    3 завданння

--- дз.py
+++ дз.py
@@ -1,9 +1,13 @@
-#2
-def fact(func):
-    def warapper(*args,**kwargs):
-        print(24+100)
-    return warapper
-@fact
-def fuction(a,b,c):
-    return a*b*c
-fuction(1,2,1)
+#3
+def logg_call(func):
+    def wrapper(*args,**kwargs):
+        returning = func(*args,**kwargs)
+        print(f"Функція приймає фргементи {args},та кваргс {kwargs} та повертає {returning}")
+        return returning
+    return wrapper
+@logg_call
+def temperature(C):
+    return (C*1.8)+32
+print(temperature(32))
+print(temperature(40))
+print(temperature(22))
