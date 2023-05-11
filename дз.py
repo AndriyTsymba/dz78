8a6477e4ca53feef87845169145cf7057f911ff1
Kommandor <a.tsymbaliuk2007@gmail.com>
2023-05-11 23:53:43 +0300

    1 завданння

---
+++ дз.py
@@ -0,0 +1,15 @@
+#1
+import time
+def time(func):
+    def wrapper(*args,**kwargs):
+        returning = func(*args,**kwargs)
+        for i in range(1,4):
+            if 0<i<4:
+                time.sleep(2)
+                print(returning)
+            return returning
+        return wrapper
+@time
+def fuction(a):
+    return a
+fuction("Light off")
